bb012bfed07dde52bedc5595748c72309e5611d5
Oppadnasser <oppadnasser@gmail.com>
2025-02-11 00:07:53 +0200

    add reseravtion repo

--- src/backend/Roomly/src/main/java/org/example/roomly/RoomlyApplication.java
+++ src/backend/Roomly/src/main/java/org/example/roomly/RoomlyApplication.java
@@ -2,7 +2,9 @@ package org.example.roomly;
 
 import org.example.roomly.model.Payment;
 import org.example.roomly.model.PaymentStatus;
+import org.example.roomly.model.Reservation;
 import org.example.roomly.repository.PaymentRepo;
+import org.example.roomly.repository.ReservationRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -26,9 +28,14 @@ public class RoomlyApplication {
     }
     public void saveReview(JdbcTemplate jdbcTemplate){
         System.out.println("hello");
-        PaymentRepo paymentRepo = new PaymentRepo(jdbcTemplate);
-//        Payment payment = new Payment();
-        paymentRepo.save("1234",new Date(2000,2,2),"cash",575, PaymentStatus.COMPLETED,"39783");
+        ReservationRepository reservationRepository = new ReservationRepository(jdbcTemplate);
+//        reservationRepository.save("39783",new Date(2003,4,4),new Date(2003,4,5),new Date(2003,4,6),
+//                "wait",500.0);
+//        PaymentRepo paymentRepo = new PaymentRepo(jdbcTemplate);
+////        Payment payment = new Payment();
+//        paymentRepo.save("1234",new Date(2000,2,2),"cash",575, PaymentStatus.COMPLETED,"39783");
+        Reservation reservation = reservationRepository.find("39783");
+        System.out.println(reservation.getStatus());
         System.out.println("saved");
     }
 }
