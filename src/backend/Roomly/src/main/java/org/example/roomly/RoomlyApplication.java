1c883113c0db9dc0aae35c077e5d91b09ee54697
Oppadnasser <oppadnasser@gmail.com>
2025-02-10 23:38:26 +0200

    add payment repo

--- src/backend/Roomly/src/main/java/org/example/roomly/RoomlyApplication.java
+++ src/backend/Roomly/src/main/java/org/example/roomly/RoomlyApplication.java
@@ -1,8 +1,15 @@
 package org.example.roomly;
 
+import org.example.roomly.model.Payment;
+import org.example.roomly.model.PaymentStatus;
+import org.example.roomly.repository.PaymentRepo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class RoomlyApplication {
@@ -11,13 +18,17 @@ public class RoomlyApplication {
         SpringApplication.run(RoomlyApplication.class, args);
     }
 
-
-    public CommandLineRunner commandLineRunner(){
+    @Bean
+    public CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate){
         return runner ->{
-                saveReview();
+                saveReview(jdbcTemplate);
         };
     }
-    public void saveReview(){
+    public void saveReview(JdbcTemplate jdbcTemplate){
         System.out.println("hello");
+        PaymentRepo paymentRepo = new PaymentRepo(jdbcTemplate);
+//        Payment payment = new Payment();
+        paymentRepo.save("1234",new Date(2000,2,2),"cash",575, PaymentStatus.COMPLETED,"39783");
+        System.out.println("saved");
     }
 }
