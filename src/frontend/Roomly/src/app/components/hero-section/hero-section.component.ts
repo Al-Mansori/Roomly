147ef456d25b78a684015333d0bdd6f232ea5b65
tayseer28 <tayseerabdelkader5@gmail.com>
2025-05-05 16:27:37 +0300

    create hero-section (not completed yet)

---
+++ src/frontend/Roomly/src/app/components/hero-section/hero-section.component.ts
@@ -0,0 +1,14 @@
+import { AfterViewInit, Component , ElementRef, OnDestroy, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-section.component.html',
+  styleUrl: './hero-section.component.scss'
+})
+export class HeroSectionComponent{
+
+
+
+}
