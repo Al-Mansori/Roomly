1b11e75e56f24891e64ce88db0db6d3edd84f232
tayseer28 <tayseerabdelkader5@gmail.com>
2025-05-05 16:26:41 +0300

    create about-us , navbar components

--- src/frontend/Roomly/src/app/app.component.ts
+++ src/frontend/Roomly/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavAuthComponent } from './components/nav-auth/nav-auth.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { AboutUsComponent } from "./components/about-us/about-us.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavAuthComponent, HomeComponent, HeroSectionComponent, AboutUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
