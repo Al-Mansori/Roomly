565e6254ffb8c0e2b282d7f8a7d8fb246583990f
tayseer28 <tayseerabdelkader5@gmail.com>
2025-05-05 16:54:20 +0300

    rename the about-us to contact-us

--- src/frontend/Roomly/src/app/app.component.ts
+++ src/frontend/Roomly/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavAuthComponent } from './components/nav-auth/nav-auth.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
-import { AboutUsComponent } from "./components/about-us/about-us.component";
+import {ContactUsComponent } from "./components/contact-us/contact-us.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavAuthComponent, HomeComponent, HeroSectionComponent, AboutUsComponent],
+  imports: [RouterOutlet, NavAuthComponent, HomeComponent, HeroSectionComponent, ContactUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
