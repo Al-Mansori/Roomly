f4bc2773e9aecd6af47b53dee1c00c7a5ae952c3
tayseer28 <tayseerabdelkader5@gmail.com>
2025-05-06 02:52:54 +0300

    create footer and about-us section (not completed yet)

--- src/frontend/Roomly/src/app/app.component.ts
+++ src/frontend/Roomly/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { NavAuthComponent } from './components/nav-auth/nav-auth.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
 import {ContactUsComponent } from "./components/contact-us/contact-us.component";
+import {AboutUsComponent } from "./components/about-us/about-us.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavAuthComponent, HomeComponent, HeroSectionComponent, ContactUsComponent],
+  imports: [RouterOutlet, NavAuthComponent, HomeComponent, HeroSectionComponent, ContactUsComponent, AboutUsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
