529ce99cb661586311b0533c1881e2678d8d0900
Mostafa Ali <92469012+Mostafadars@users.noreply.github.com>
2025-06-24 02:17:24 +0300

    Update app_api.dart

--- src/mobile/roomly/lib/core/network/app_api.dart
+++ src/mobile/roomly/lib/core/network/app_api.dart
@@ -1,4 +1,4 @@
 class AppApi {
-  // static const String baseUrl = 'https://feminist-abigael-roomly-5d3753ef.koyeb.app';
-  static const String baseUrl = 'http://192.168.1.46:8080';
+  static const String baseUrl = 'https://feminist-abigael-roomly-5d3753ef.koyeb.app';
+  // static const String baseUrl = 'http://192.168.1.46:8080';
 }
