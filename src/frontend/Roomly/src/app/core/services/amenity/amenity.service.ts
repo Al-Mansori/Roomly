0f5a27e2a080501546011afd5e059dd96db2c14a
tayseer28 <tayseerabdelkader5@gmail.com>
2025-07-08 01:39:19 +0300

    fix amenities conflict

--- src/frontend/Roomly/src/app/core/services/amenity/amenity.service.ts
+++ src/frontend/Roomly/src/app/core/services/amenity/amenity.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { IAmenity, IAmenityResponse } from '../../../interfaces/iamenity';
+import { IAmenity, IAmenityResponse, IExtendedAmenity } from '../../../interfaces/iamenity';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environment/environments';
@@ -33,7 +33,32 @@ export class AmenityService {
       { params }
     );
   }
-}
 
+  updateAmenity(amenityData: IExtendedAmenity): Observable<IAmenityResponse> {
+    let params = new HttpParams()
+      .set('amenityId', amenityData.id || '')
+      .set('name', amenityData.name)
+      .set('type', amenityData.type)
+      .set('description', amenityData.description)
+      .set('totalCount', amenityData.totalCount.toString())
+      .set('staffId', amenityData.staffId);
+
+    if (amenityData.imageUrls && amenityData.imageUrls.length > 0) {
+      amenityData.imageUrls.forEach((url) => {
+        params = params.append('imageUrls', url);
+      });
+    }
+
+    return this.http.put<IAmenityResponse>(
+      `/api/staff/update-amenity`,
+      {},
+      { params }
+    );
+  }
+
+  getAmenityById(amenityId: string): Observable<IExtendedAmenity> {
+    return this.http.get<IExtendedAmenity>(`/api/staff/amenity/${amenityId}`);
+  }
+}
 
 
